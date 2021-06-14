7b9b79c9e7b15c1e1d610429ebf6be33c720df2a
Victor Isidoro <victorisidoro@icloud.com>
2021-06-14 18:27:04 +0100

    Name updated according to criteria

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const personData = require('./information.js')
+const personData = require('./information')
 const cowsay = require('cowsay')
 
 console.log(cowsay.say({text:`Hello, my name is ${personData.name} and I'm from ${personData.campus}`, e: '-O', T:'U'}))
