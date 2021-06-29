7c28e36cc90d90439490b4d37bddd649186f7045
Victor Isidoro <victorisidoro@icloud.com>
2021-06-29 09:12:35 +0100

    ESLINT test

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const personData = require('./information')
-const cowsay = require('cowsay')
+const cowsay = require('cowsay');
+const personData = require('./information');
 
-console.log(cowsay.say({text:`Hello, my name is ${personData.name} and I'm from ${personData.campus}`, e: '-O', T:'U'}))
+console.log(cowsay.say({ text: `Hello, my name is ${personData.name} and I'm from ${personData.campus}`, e: '-O', T: 'U' }));
